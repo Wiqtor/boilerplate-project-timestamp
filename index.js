9508cd782e2dbb17d906ef6e2ea1f79abb349185
Victor Rodrigues <victor@netprima.com.br>
2023-08-01 20:26:06 -0300

    first commit

--- index.js
+++ index.js
@@ -2,31 +2,51 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
 
+app.get("/api", (req, res) => {
+  let now = new Date();
+  res.json({ unix: now.valueOf(), utc: now.toUTCString() });
+});
 
+app.get("/api/:date?", (req, res) => {
+  let param = req.params.date;
+  let resDate;
+  let isReadable = /[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])/.test(
+    param
+  );
+  console.log(isReadable);
+  resDate = isReadable
+    ? new Date(Date.parse(param))
+    : new Date(parseInt(param));
+  res.json({
+    unix: resDate.valueOf(),
+    utc: resDate.toUTCString(),
+    // input: param,
+    // resDate: resDate,
+  });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
